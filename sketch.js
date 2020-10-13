58569a88034c52e12fc34983f840b0d5fe0e626a
yashvardhan2009 <72388682+yashvardhan2009@users.noreply.github.com>
2020-10-13 15:18:39 +0530

    CAR
    THE_ANIMATION

--- sketch.js
+++ sketch.js
@@ -1,146 +1,54 @@
+var car,carImage;
 
-var PLAY=1;
-var END=0;
-var gameState=1;
-
-var sword,fruit ,monster,fruitGroup,enemyGroup, score,r,randomFruit, position;
-var swordImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage
-var gameOverSound ,knifeSwoosh
+var wall;
 
 function preload(){
-  
-  swordImage = loadImage("sword.png");
-  monsterImage = loadAnimation("alien1.png","alien2.png")
-  fruit1 = loadImage("fruit1.png");
-  fruit2 = loadImage("fruit2.png");
-  fruit3 = loadImage("fruit3.png");
-  fruit4 = loadImage("fruit4.png");
-  gameOverImage = loadImage("gameover.png")
-  
-  gameOverSound = loadSound("game_over.mp3")
-  knifeSwooshSound = loadSound("knife.mp3")
+
+carImage = loadImage("car1.png");
+
+
+
 }
 
-
-
 function setup() {
-  createCanvas(600, 600);
-  
-  //creating sword
-   sword=createSprite(40,200,20,20);
-   sword.addImage(swordImage);
-   sword.scale=0.7
-  
-  
-  
-  
-  sword.setCollider("rectangle",0,0,40,40);
+  createCanvas(600,600);
+  car = createSprite(35, 290, 10,10);
+  car.addImage(carImage);
+  car.scale = 0.1;
+  car.shapeColor = ("yellow")
 
-  
-  score=0;
-  fruitGroup=createGroup();
-  enemyGroup=createGroup();
-  
+  wall = createSprite(580,300,50,600);
+  wall.shapeColor = "green";
+
 }
 
 function draw() {
-  background("lightblue");
-  
-  
-    
+  background(170); 
 
+  console.log(car.x)
   
-  
-  if(gameState===PLAY){
-    
-    //Call fruits and Enemy function
-    fruits();
-    Enemy();
-    
-    // Move sword with mouse
-    sword.y=World.mouseY;
-    sword.x=World.mouseX;
-  
-    
-    if(fruitGroup.isTouching(sword)){
-      fruitGroup.destroyEach();
-      
-      knifeSwooshSound.play();
-      score=score+2;
-    }
-    else
-    {
-      
-      if(enemyGroup.isTouching(sword)){
-        gameState=END;
-        //gameover sound
-        gameOverSound.play()
-        
-        fruitGroup.destroyEach();
-        enemyGroup.destroyEach();
-        fruitGroup.setVelocityXEach(0);
-        enemyGroup.setVelocityXEach(0);
-        
-        
-        sword.addImage(gameOverImage);
-        sword.x=300;
-        sword.y=300;
-      }
-    }
+  if(keyDown(RIGHT_ARROW)){
+
+    car.velocityX = 3.5;
+
   }
-  
+
+if (car.x >  535)  {
+
+car.x = 535;
+
+}
+
+  if (wall.x - car.x < (wall.width + car.width)/10) {
+ 
+      wall.shapeColor = "red";
+      car.x = 35;
+      car.velocityX = 0;
+      
+    }
+else {
+  wall.shapeColor = "grey";
+}
+
   drawSprites();
-  
-  
-  text("Score : "+ score,300,30);
-}
-
-
-function Enemy(){
-  if(World.frameCount%200===0){
-    monster=createSprite(400,200,20,20);
-    monster.addAnimation("moving", monsterImage);
-    monster.y=Math.round(random(100,300));
-    monster.velocityX=-(8+(score/10));
-    monster.setLifetime=50;
-    
-    enemyGroup.add(monster);
-  }
-}
-
-function fruits(){
-  if(World.frameCount%80===0){
-    position = Math.round(random(1,2));
-    fruit=createSprite(400,200,20,20);
-    console.log(position)
-     
-   
-    if(position===1)
-    {
-    
-    fruit.x=400;
-    fruit.velocityX=7;
-    console.log(fruit.velocity)
-    }
-
-    fruit.scale=0.2;
-     
-     r=Math.round(random(1,4));
-    if (r == 1) {
-      fruit.addImage(fruit1);
-    } else if (r == 2) {
-      fruit.addImage(fruit2);
-    } else if (r == 3) {
-      fruit.addImage(fruit3);
-    } else {
-      fruit.addImage(fruit4);
-    }
-    
-    fruit.y=Math.round(random(50,340));
-   
-    fruit.velocityX=-7;
-    fruit.setLifetime=100;
-    
-    fruitGroup.add(fruit);
-  }
 }
